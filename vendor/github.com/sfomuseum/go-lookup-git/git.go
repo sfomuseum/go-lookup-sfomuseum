16429395f04e7a2b7dd02a41047a90613c9720a3
thisisaaronland <devnull@localhost>
2020-01-02 17:50:22 -0800

    update go-lookup-git

--- vendor/github.com/sfomuseum/go-lookup-git/git.go
+++ vendor/github.com/sfomuseum/go-lookup-git/git.go
@@ -14,11 +14,15 @@ import (
 type GitLookerUpper struct {
 	lookup.LookerUpper
 	uri string
+	ref string
 }
 
 func NewGitLookerUpper(ctx context.Context) lookup.LookerUpper {
 
-	l := &GitLookerUpper{}
+	l := &GitLookerUpper{
+		ref: "refs/remotes/origin/master",
+	}
+
 	return l
 }
 
@@ -30,7 +34,7 @@ func (l *GitLookerUpper) Open(ctx context.Context, uri string) error {
 func (l *GitLookerUpper) Append(ctx context.Context, lu lookup.Catalog, append_funcs ...lookup.AppendLookupFunc) error {
 
 	r, err := gogit.Clone(memory.NewStorage(), nil, &gogit.CloneOptions{
-		URL: l.uri,
+		URL:   l.uri,
 		Depth: 1,
 	})
 
@@ -38,15 +42,30 @@ func (l *GitLookerUpper) Append(ctx context.Context, lu lookup.Catalog, append_f
 		return err
 	}
 
-	it, err := r.BlobObjects()
+	// please fix me...
+	// ./git.go:45:27: cannot use l.ref (type string) as type plumbing.ReferenceName in argument to r.Reference
+
+	ref, err := r.Reference("refs/remotes/origin/master", true)
 
 	if err != nil {
 		return err
 	}
 
-	err = it.ForEach(func(bl *object.Blob) error {
+	commit, err := r.CommitObject(ref.Hash())
 
-		fh, err := bl.Reader()
+	if err != nil {
+		return err
+	}
+
+	files, err := commit.Files()
+
+	if err != nil {
+		return err
+	}
+
+	err = files.ForEach(func(f *object.File) error {
+
+		fh, err := f.Reader()
 
 		if err != nil {
 			return err
@@ -60,15 +79,15 @@ func (l *GitLookerUpper) Append(ctx context.Context, lu lookup.Catalog, append_f
 			return err
 		}
 
-		for _, f := range append_funcs {
+		for _, append_f := range append_funcs {
 
 			br := bytes.NewReader(body)
 			fh := ioutil.NopCloser(br)
 
-			err := f(ctx, lu, fh)
+			err := append_f(ctx, lu, fh)
 
 			if err != nil {
-				log.Printf("GIT %s: %s\n", bl.Hash, err)
+				log.Printf("GIT %s: %s\n", f.Name, err)
 				// return err
 			}
 		}
